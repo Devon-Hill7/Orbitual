89f991454bfbe501f2c70a31f2b0f2efb2b3f311
Devon-Hill7 <devon17@ksu.edu>
2026-05-02 14:13:12 -0500

    Initial commit

---
+++ orbitual/src/main/java/com/devon/orbitual/OrbitualApplication.java
@@ -0,0 +1,13 @@
+package com.devon.orbitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitualApplication.class, args);
+	}
+
+}
---
+++ orbitual/src/test/java/com/devon/orbitual/OrbitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devon.orbitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
