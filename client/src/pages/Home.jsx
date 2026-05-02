5d37774976345548ab06156b09322b7e884376ea
Devon-Hill7 <devon17@ksu.edu>
2026-05-02 16:26:40 -0500

    basic deploymeny setup for frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     const [serverHealth, setServerHealth] = useState('Checking...');
 
     useEffect(() => {
-        axios.get(process.env.REACT_APP_API_URL + '/health')
+        axios.get(`${process.env.REACT_APP_API_URL}/health`)
             .then(res => setServerHealth(res.data))
             .catch(() => setServerHealth('FAILED'));
     }, []);
